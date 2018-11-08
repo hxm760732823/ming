75fe4652370cf26d45ed0827c9377291df578dc1
ming <760732823@qq.com>
2018-11-08 17:26:44 +0800

    提交

--- src/main/java/com/ming/MingApplication.java
+++ src/main/java/com/ming/MingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MingApplication.class, args);
     }
