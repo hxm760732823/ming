4ecea9acae090c06aef7464c09b1b25a1e6dad1b
ming <760732823@qq.com>
2018-11-29 15:49:58 +0800

    提交dcad

--- src/main/java/com/ming/MingApplication.java
+++ src/main/java/com/ming/MingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MingApplication {
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(MingApplication.class, args);
     }
 }
